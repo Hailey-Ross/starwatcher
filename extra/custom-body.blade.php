3c2fe41b4251f248fb0094bb729e57a1a4226953
Hails <26784904+Hailey-Ross@users.noreply.github.com>
2023-09-10 19:17:02 -0600

    Update custom-body.blade.php

--- extra/custom-body.blade.php
+++ extra/custom-body.blade.php
@@ -1,9 +1,3 @@
-<style> /* This is a comment */ </style>
-
-<script> // This is a comment </script>
-
 <div id="particles-js"></div>
 <script src="https://assets.hails.cc\particlejs\particles.js"></script>
 <script src="https://assets.hails.cc\particlejs\script.js"></script>
-
-<?php // This is a comment ?>
